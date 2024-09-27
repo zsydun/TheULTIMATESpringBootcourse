231cedd020d9f46e0d7ebc1553a79e661979e4b5
Zhenya <zsydun@gamil.com>
2024-09-27 14:27:50 +0300

    first commit

---
+++ src/main/java/course/theultimatespringbootcourse/TheUltimateSpringBootCourseApplication.java
@@ -0,0 +1,13 @@
+package course.theultimatespringbootcourse;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TheUltimateSpringBootCourseApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TheUltimateSpringBootCourseApplication.class, args);
+    }
+
+}
---
+++ src/test/java/course/theultimatespringbootcourse/TheUltimateSpringBootCourseApplicationTests.java
@@ -0,0 +1,13 @@
+package course.theultimatespringbootcourse;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TheUltimateSpringBootCourseApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
